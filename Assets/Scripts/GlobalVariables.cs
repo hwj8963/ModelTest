5ae41ae2b86baacc7ea7d7ade052f16449f5f7a4
hwj8963 <hwj8963@gmail.com>
2015-10-01 20:01:55 +0900

    원거리 미니언 프리팹 추가

---
+++ Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalVariables : ScriptableObject {
+
+    public static readonly int FirstBloodScore = 400;
+    public static readonly int BasicCharacterScore = 300;
+    public static readonly int MeleeMinionScoreBase = 18;
+    public static readonly int MeleeMinionScorePerLevel = 1;
+    public static readonly int CasterMinionScoreBase = 13;
+    public static readonly int CasterMinionScorePerLevel = 1;
+    public static readonly int SiegeMinionScoreBase = 38;
+    public static readonly int SiegeMinionScorePerLevel = 2;
+}
